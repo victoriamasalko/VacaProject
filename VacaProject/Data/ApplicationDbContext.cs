7cbb33e212cb158e7eaac6d72c16b27e43c6ebf6
Victoria Masalkovski <victoria.masalkovski@gmail.com>
2026-06-29 21:58:26 +0200

    Stop tracking my-directory

--- VacaProject/Data/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
-using Microsoft.EntityFrameworkCore;
-
-namespace VacaProject.Data
-{
-    public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext(options)
-    {
-    }
-}
--- VacaProject/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace VacaProject.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
